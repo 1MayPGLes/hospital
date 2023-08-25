504d94144b11038db31393bdea4e2ce80bb03549
1_May <pelevinig525@gmail.com>
2023-08-25 05:41:18 +0300

    Сделал свайпер с картами врачей. Основная часть фронта готова

---
+++ js/wrapper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 1000,
+    centeredSlides: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+
+    autoplay: {
+        delay: 5000,
+    },
+  });

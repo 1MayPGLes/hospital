1fe4895dc1a99bd42db4d8176c3857c873c49642
1_May <pelevinig525@gmail.com>
2023-09-04 20:38:42 +0300

    Поправил пару ошибок

--- js/wrapper.js
+++ js/wrapper.js
@@ -3,10 +3,9 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     speed: 1000,
     centeredSlides: true,
-    spaceBetween: 10,
     slidesPerView: 3,
 
     autoplay: {
         delay: 5000,
     },
-  });
+});
